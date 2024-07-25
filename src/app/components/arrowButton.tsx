b76d73851c6bb1e19b9304a9da3a3e96e76da393
Javed Chaudhary <chaudharyjavedahmed@gmail.com>
2024-07-26 01:01:57 +0530

    services section

---
+++ src/app/components/arrowButton.tsx
@@ -0,0 +1,20 @@
+import { FaArrowRight } from "react-icons/fa";
+
+interface ArrowButtonProps {
+    name: string;
+    url?: string;
+}
+
+const ArrowButton: React.FC<ArrowButtonProps> = ({ name, url }) => {
+    return(
+        <button className='py-4  group hover:bg-gray-50 hover:pr-7 rounded-xl drop-shadow-md'>
+        <div className='text-lg flex items-center gap-2 font-semibold text-blue-600 transition-custom transform group-hover:translate-x-4'>
+        <p>{name}</p>
+        <FaArrowRight className="size-4" />
+        </div>
+        </button>
+    )
+};
+
+export default ArrowButton
+
--- src/app/components/services.tsx
+++ src/app/components/services.tsx
@@ -1,16 +1,67 @@
-import Image from "next/image"
-export default function Services(){
-    return(
-        <div>
-            <div className="h-[630px] w-[630px]">
+import Image from "next/image";
+import ArrowButton from "./arrowButton";
+
+export default function Services() {
+  return (
+    <div>
+      {serviceData.map((d, index) => (
+        <div
+          key={index}
+          className={`flex gap-4 items-center justify-between ${
+            index % 2 !== 0 ? "flex-row-reverse" : ""
+          }`}
+        >
+          <div className="h-[530px] w-[630px]">
             <Image
-              src={"/webdev1.svg"}
-              alt="discussion"
+              src={d.service_img}
+              alt={d.service_name}
               width={500}
               height={500}
-              className="object-contain w-full h-full"
+              className={`object-${d.service_img_fit} w-full h-full`}
             />
+          </div>
+
+          <div className="w-[50%] px-6">
+            <h1 className="service-name justify-start text-5xl font-extrabold w-full py-6">
+              {d.service_name.split("").map((char, index) => (
+                <span key={index} className="char">
+                  {char}
+                </span>
+              ))}
+            </h1>
+
+            <p className="text-lg font-normal pr-6">{d.service_description}</p>
+            <ArrowButton name={d.service_button_name} />
+          </div>
         </div>
-        </div>
-    )
+      ))}
+    </div>
+  );
 }
+
+const serviceData = [
+  {
+    service_name: "Web Devopelopment",
+    service_img: "/webdev1.svg",
+    service_img_fit: "cover",
+    service_description:
+      "We develop all kinds of websites and web apps, including complex features like e-commerce, interactive graphs, chat systems, and more.",
+    service_button_name: "learn more about our web development",
+  },
+  {
+    service_name: "App Devopelopment",
+    service_img: "/app_development.png",
+    service_img_fit: "contain",
+    service_description:
+      "We create a wide range of mobile applications for both Android and iOS, incorporating advanced features such as e-commerce, interactive graphs, chat systems, and more.",
+    service_button_name: "learn more about our App development",
+  },
+  {
+    service_name: "Automation",
+    service_img: "/automation.svg",
+    service_img_fit: "cover",
+    service_description:
+      "We can automate your manual processes using Python, delivering efficient solutions to streamline your operations and increase productivity.",
+    service_button_name: "learn more about Automation",
+  },
+];
