53f4bc43d437d898c8cdc71fdd714bdf37a30428
Javed Chaudhary <chaudharyjavedahmed@gmail.com>
2024-07-28 22:48:48 +0530

    minor changes in app and web development page

--- src/app/app-development/page.tsx
+++ src/app/app-development/page.tsx
@@ -1,92 +1,107 @@
 import Image from "next/image";
-import ArrowButton from "../components/arrowButton";
 
-export default function webdevelopment() {
+export default function AppDevelopment() {
   return (
-    <div className="w-full px-4 lg:w-[90%] flex flex-col gap-14 bg-slate-100 rounded-lg">
-        <h1 className="text-5xl font-sans font-bold mx-auto my-5 text-indigo-800">These are few projects we worked on in past</h1>
+    <div className="w-full px-4 lg:w-[90%] flex flex-col gap-14 bg-slate-100 rounded-lg text-center">
+      <h1 className="text-3xl sm:text-5xl font-extrabold mx-auto my-5 blue-text-gradient ">
+        App development services we provide
+      </h1>
       {serviceData.map((d, index) => (
-        <div
+        <section
           key={index}
           className={`flex gap-8 lg:gap-4 items-center flex-col lg:flex-row justify-between ${
             index % 2 !== 0 ? "lg:flex-row-reverse" : ""
           }`}
+          aria-label={`${d.service_name} service discription`}
         >
-          <div className="h-auto lg:h-[530px] w-[80%] lg:w-[630px]">
+          <figure className="h-auto lg:h-[530px] w-[80%] lg:w-[630px]">
             <Image
               src={d.service_img}
               alt={d.service_name}
               width={400}
               height={400}
               className={`object-${d.service_img_fit} w-full h-full mix-blend-darken`}
+              aria-label={`${d.service_name} illustration image`}
             />
-          </div>
+          </figure>
 
-          <div className="w-full md:w-[70%] lg:w-[50%] md:px-6 text-center md:text-start">
-            <h1 className="hidden xl:block service-name justify-start text-5xl font-extrabold w-full py-6 ">
-              {d.service_name.split("").map((char, index) => (
-                <span key={index} className="char">
-                  {char === " " ? (
-                    <span className="space-char hidden xl:block">&nbsp;</span>
-                  ) : (
-                    char
-                  )}
-                </span>
-              ))}
-            </h1>
+          <article className="w-full md:w-[70%] lg:w-[50%] md:px-6 text-center md:text-start">
+            <header>
+              <h1 className="hidden xl:block service-name justify-start text-5xl font-extrabold w-full ">
+                {d.service_name.split("").map((char, index) => (
+                  <span key={index} className="char">
+                    {char === " " ? (
+                      <span className="space-char hidden xl:block">&nbsp;</span>
+                    ) : (
+                      char
+                    )}
+                  </span>
+                ))}
+              </h1>
 
-            <h1 className="block xl:hidden text-3xl md:text-5xl font-extrabold break-words py-6">{d.service_name}</h1>
+              <h1 className="block xl:hidden text-3xl md:text-5xl font-extrabold break-words py-6">
+                {d.service_name}
+              </h1>
+            </header>
 
             <p className="text-lg font-normal pr-6">{d.service_description}</p>
-          </div>
-        </div>
+          </article>
+        </section>
       ))}
+
+      <section className="text-center flex flex-col gap-3 mb-6 py-10">
+        <h2 className="text-2xl sm:text-4xl font-bold blue-text-gradient ">
+          Can't find the services you need?
+        </h2>
+        <h2 className="text-lg sm:text-3xl">
+          no worries reach out to us and we can make it happen
+        </h2>
+      </section>
     </div>
   );
 }
 
 const serviceData = [
-    {
-      service_name: "E-Commerce Platform",
-      service_img: "/webdevelopment/ecommerce.svg",
-      service_img_fit: "cover",
-      service_description:
-        "We build robust e-commerce platforms that drive sales and enhance user experience, helping clients boost their online business.",
-    },
-    {
-      service_name: "Mobile App Maintenance",
-      service_img: "/webdevelopment/maintaince.svg",
-      service_img_fit: "contain",
-      service_description:
-        "We ensure your mobile App is always up-to-date and running smoothly, allowing clients to focus on their core business without technical worries.",
-    },
-    {
-      service_name: "UI/UX",
-      service_img: "/webdevelopment/UIUX.svg",
-      service_img_fit: "cover",
-      service_description:
-        "We design intuitive and engaging user interfaces that improve customer satisfaction and retention, transforming client visions into reality.",
-    },
-    {
-      service_name: "Dashboard",
-      service_img: "/webdevelopment/dashboard.svg",
-      service_img_fit: "cover",
-      service_description:
-        "We create comprehensive dashboards that provide real-time insights, helping clients make informed decisions and streamline operations.",
-    },
-    {
-      service_name: "SEO",
-      service_img: "/webdevelopment/SEO.svg",
-      service_img_fit: "cover",
-      service_description:
-        "We implement effective SEO strategies that increase visibility and drive organic traffic, significantly improving clients' online presence.",
-    },
-    {
-      service_name: "Chat System",
-      service_img: "/webdevelopment/chat.svg",
-      service_img_fit: "cover",
-      service_description:
-        "We develop advanced chat systems that enhance customer engagement and support, providing clients with seamless communication solutions.",
-    },
-  ];
-  
+  {
+    service_name: "E-Commerce Platform",
+    service_img: "/webdevelopment/ecommerce.svg",
+    service_img_fit: "contain",
+    service_description:
+      "We build robust e-commerce platforms that drive sales and enhance user experience, helping clients boost their online business.",
+  },
+  {
+    service_name: "Mobile App Maintenance",
+    service_img: "/webdevelopment/maintaince.svg",
+    service_img_fit: "contain",
+    service_description:
+      "We ensure your mobile App is always up-to-date and running smoothly, allowing clients to focus on their core business without technical worries.",
+  },
+  {
+    service_name: "UI/UX",
+    service_img: "/webdevelopment/UIUX.svg",
+    service_img_fit: "cover",
+    service_description:
+      "We design intuitive and engaging user interfaces that improve customer satisfaction and retention, transforming client visions into reality.",
+  },
+  {
+    service_name: "Dashboard",
+    service_img: "/webdevelopment/dashboard.svg",
+    service_img_fit: "cover",
+    service_description:
+      "We create comprehensive dashboards that provide real-time insights, helping clients make informed decisions and streamline operations.",
+  },
+  {
+    service_name: "SEO",
+    service_img: "/webdevelopment/SEO.svg",
+    service_img_fit: "cover",
+    service_description:
+      "We implement effective SEO strategies that increase visibility and drive organic traffic, significantly improving clients' online presence.",
+  },
+  {
+    service_name: "Chat System",
+    service_img: "/webdevelopment/chat.svg",
+    service_img_fit: "cover",
+    service_description:
+      "We develop advanced chat systems that enhance customer engagement and support, providing clients with seamless communication solutions.",
+  },
+];
--- src/app/components/arrowButton.tsx
+++ src/app/components/arrowButton.tsx
@@ -2,21 +2,29 @@ import Link from "next/link";
 import { FaArrowRight } from "react-icons/fa";
 
 interface ArrowButtonProps {
-    name: string;
-    url?: string;
+  name: string;
+  url?: string;
 }
 
 const ArrowButton: React.FC<ArrowButtonProps> = ({ name, url }) => {
-    const validUrl = url || '#'; // Fallback to '#' if url is undefined
+  const validUrl = url || "#"; // Fallback to '#' if url is undefined
 
-    return (
-        <Link href={validUrl} className='py-4 group hover:bg-gray-50 hover:pr-7 rounded-xl drop-shadow-md' aria-label={name}>
-            <div className='text-lg flex flex-col sm:flex-row items-center gap-2 font-semibold text-blue-600 transition-custom transform group-hover:translate-x-4'>
-                <p>{name}</p>
-                <FaArrowRight className="size-4" />
-            </div>
-        </Link>
-    );
+  return (
+    <Link
+      href={validUrl}
+      aria-label={name}
+    >
+      <button
+        className="py-4  group hover:bg-gray-50 hover:pr-7 rounded-xl drop-shadow-md"
+        aria-label={name}
+      >
+        <div className="text-lg flex flex-col sm:flex-row items-center gap-2 font-semibold text-blue-600 transition-custom transform group-hover:translate-x-4">
+          <p>{name}</p>
+          <FaArrowRight className="size-4" />
+        </div>
+      </button>
+    </Link>
+  );
 };
 
 export default ArrowButton;
